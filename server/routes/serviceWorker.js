33c4a2827f362df40987e4fd256303e718dd0bcd
apcesk <apcesk@163.com>
2020-08-26 20:06:15 +1200

    路由

--- server/routes/serviceWorker.js
+++ server/routes/serviceWorker.js
@@ -7,6 +7,8 @@ const ServiceWorkerRouter = {
     routes: [
         {methods: 'post', path:'/login', realize: SWC.login}, // 客服人员的登录
         {methods: 'get', path:'/getMyCustomer', realize: SWC.getMyCustomer}, // 客服人员的登录
+        {methods: 'post', path:'/addCustomer', realize: SWC.addCustomer}, // 客服人员的登录
+        {methods: 'get', path:'/getCustomerById', realize: SWC.getCustomerById}, // 客服人员的登录
     ]
 }
 
