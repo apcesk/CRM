3b2f915c1117a816cea9414e2e98faf6cd4362d6
张玉成 <zhangyucheng@zhangyuchengdeMacBook-Pro.local>
2020-12-24 18:25:50 +0800

    部署服务器的配置修改

--- server/config/index.js
+++ server/config/index.js
@@ -1,8 +1,10 @@
 // 配置相关属性
 // 是否是开发环境
-const DEV = true;
-// 配置服务器的访问地址
-const serverUrl = '...';
+const DEV = false;
+// 配置服务器的访问地址，主要是在没有域名的情况下使用
+const serverUrl = '39.104.84.48';
+// 配置上线域名,如果有域名的话使用这个
+const domain = 'test.apcesk.com';
 /**
  * @attribute port 服务器 端口号
  * @mysql mysql相关配置
@@ -14,12 +16,15 @@ const config = {
     mysql: {
         database: 'CRM',
         user: 'root',
-        password: DEV ? 'MH0703zyc1225' : '',
+        password: DEV ? 'MH0703zyc1225' : 'MH0703zyc1225',
         port: '3306',
         host: '',
     },
     prefix: '/api',
-    originUrl: DEV ? 'http://localhost:3000' : serverUrl
+    // 没有域名用这个
+    // originUrl: DEV ? 'http://localhost:3000' : serverUrl
+    // 有域名用这个
+    originUrl: DEV ? 'http://localhost:3000' : domain
 };
 
 module.exports = config;
