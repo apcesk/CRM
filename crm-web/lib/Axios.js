ada62f6e5aecb9b2bacfbd9a028e7aae9864b013
张玉成 <zhangyucheng@zhangyuchengdeMacBook-Pro.local>
2020-12-24 17:34:59 +0800

    修改部署服务器使用的url

--- crm-web/lib/Axios.js
+++ crm-web/lib/Axios.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 import User from './user';
 
 class Axios {
-    static _baseUrl = "http://localhost:8080/api/";
+    // 本地开发时使用的url
+    // static _baseUrl = "http://localhost:8080/api/";
+    // 部署时使用的请求服务器接口
+    static _baseUrl = "http://39.104.84.48:8080/api/";
     static init() {
         const config = {
             withCredentials: true,
